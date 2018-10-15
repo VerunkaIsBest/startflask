de23065dba41202d88f8f12830039637d22c6fa9
Veronika Vojackova <voj35202@lp609.spseol.cz>
2018-10-15 10:05:43 +0200

    Přidáno ABC

--- debugapp.py
+++ debugapp.py
@@ -32,6 +32,10 @@ def index():
 def onas():
     return render_template('onas.html')
 
+@app.route('/abc/')
+def abc():
+    return render_template('abc.html')
+
 
 ############################################################################
 if __name__ == '__main__':
